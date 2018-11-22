97b514def19e6bec0a4762022655c8924bf1364e
xXxYeetMasterxXx <45269150+xXxYeetMasterxXx@users.noreply.github.com>
2018-11-22 12:52:27 -0500

    Add files via upload

---
+++ Partin_Pereira_Ramirez_Notecards/Partin_Pereira_Ramirez_Notecards/ViewController.swift
@@ -0,0 +1,23 @@
+//  Partin_Pereira_Ramirez_Notecards
+//  Sam Partin, Lucas Pereira, Milton Ramirez-Flores
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print ("Testing")
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let ourCell1 = tableView.dequeueReusableCell(withIdentifier: "ProCells1", for: indexPath)
+        return ourCell1
+    }
+
+}
+
